e1a5604b892d28c7ed955bfd7539cbfd15047336
nepomn9shih <nepomn9shih@gmail.com>
2020-10-29 17:26:31 +0300

    Создал заготовку

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+        <img
+          className="logo"
+          alt="logo"
+          src="https://erickson.by/images/tild3135-3165-4566-a235-353432643538__38-instagram-3-512.png"
+        />
+      </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Content = () => {
+    return (
+      <div className="content">
+        <img
+          className="longpic"
+          src="https://upload.wikimedia.org/wikipedia/commons/9/93/Long_sky_background_%2B_PAN.jpg"
+          alt="longpic"
+        />
+        <div>
+          <img
+            className="avatar"
+            src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/cat-face-by-jonathan-fife.jpg"
+            alt="avatar"
+          />
+        </div>
+        description
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Content
