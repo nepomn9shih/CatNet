ad9d89a76a589f2e06cef1f43f259bc70f12e1b1
nepomn9shih <nepomn9shih@gmail.com>
2020-11-29 14:03:56 +0300

    Исправил баг с отображением статуса с помощью useEffect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,20 +6,20 @@ import { NavLink } from "react-router-dom";
 const Header = (props) => {
     return (
         <header className="row">
-        <div className="col-2 d-flex align-items-center">
+        <div className="col-3 d-flex align-items-center">
           <img
             className="logo"
             alt="logo"
             src={logo}
           />
         </div>
-        <div className="col-8 d-flex justify-content-center align-items-center">
+        <div className="col-6 d-flex justify-content-center align-items-center">
           <h1 className="text-white">CatNet</h1>
         </div>
-        <div className="col-2 d-flex align-items-center link">
+        <div className="col-3 d-flex justify-content-end align-items-center link pr-3">
         {props.isAuth 
         ? <div>{props.login} <button onClick={props.logoutThunkCreator} className="btn btn-warning btn-sm">Log out</button></div> 
-        : <NavLink to='/login'>Log in</NavLink>}
+        : <NavLink to='/login'><button className="btn btn-warning btn-sm">Log in</button></NavLink>}
         </div>
       </header>
     )
