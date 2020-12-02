22a93febbb47f6aee20decdbdacfc6fce09ac529
nepomn9shih <nepomn9shih@gmail.com>
2020-12-02 19:12:09 +0300

    Поменял браузерроутер на хэшроутер, чтобы обновлялось на Git Pages

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import store from './redux/reduxStore'
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+// ReactDOM.render(
+//   <BrowserRouter basename={process.env.PUBLIC_URL}>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
