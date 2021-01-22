1f82b5692e00728b8a409b09f0ca4329d54dab6e
nepomn9shih <nepomn9shih@gmail.com>
2021-01-22 14:25:30 +0300

    Перевел на TypeScript profileReducer

--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -7,18 +7,52 @@ const SET_STATUS = "profile/SET_STATUS"
 const DELETE_POST = "profile/DELETE_POST";
 const SAVE_PHOTO_SUCCESS = "profile/SAVE_PHOTO_SUCCESS"
 
+type PostType = {
+id: number
+message: string
+likeCount: number
+}
+
+type ContactsType = {
+  github: string
+  vk: string
+  facebook: string
+  instagram: string
+  twitter: string
+  website: string
+  youtube: string
+  mainlink: string
+}
+
+type PhotosType = {
+  small: string | null
+  large: string | null
+}
+
+type ProfileType = {
+userId: number
+lookingForAJob: boolean
+lookingForAJobDescription: string
+fullName: string
+contacts: ContactsType
+photos: PhotosType
+}
+
 let initialState = {
   posts: [
     { id: 1, message: "Опыт - это то, что позволяет человеку, сделав ошибку, сказать: 'Я так и знал!'", likeCount: 99 },
     { id: 2, message: "В мире еще много граблей, на которые не ступала нога человека!", likeCount: 21 },
     { id: 3, message: "Начиная новое дело, надо быть готовым даже к тому, что у тебя все получится", likeCount: 8 },
     { id: 4, message: "Что недопустимо в принципе, часто необходимо на практике.", likeCount: 6 },
-  ],
-  profile: null,
-  status: ""
+  ] as Array<PostType>,
+  profile: null as ProfileType | null,
+  status: "" as string,
+  newPostText: "" as string | null
 };
 
-const profileReducer = (state = initialState, action) => {
+export type PostInitialStateType = typeof initialState
+
+const profileReducer = (state = initialState, action: any):PostInitialStateType => {
   switch (action.type) {
     case ADD_POST: {
       let newPost = {
@@ -53,7 +87,7 @@ const profileReducer = (state = initialState, action) => {
     case SAVE_PHOTO_SUCCESS: {
       return {
         ...state,
-        profile: {...state.profile, photos: action.photos}
+        profile: {...state.profile, photos: action.photos} as ProfileType
       };
     }
     default:
@@ -61,42 +95,67 @@ const profileReducer = (state = initialState, action) => {
   }
 };
 
-export const addPostActionCreator = (newPostText) => ({ type: ADD_POST, newPostText });
+type AddPostActionType = {
+  type: typeof ADD_POST
+  newPostText: string | null
+}
 
-export const setUserProfileAC = (profile) => ({
+export const addPostActionCreator = (newPostText: string | null):AddPostActionType => ({ type: ADD_POST, newPostText });
+
+type SetUserProfileActionType = {
+  type: typeof SET_USER_PROFILE
+  profile: ProfileType
+}
+
+export const setUserProfileAC = (profile: ProfileType):SetUserProfileActionType => ({
   type: SET_USER_PROFILE,
   profile
 })
 
-export const setStatusAC = (status) => ({
+type SetStatusActionType = {
+  type: typeof SET_STATUS
+  status: string | null
+}
+
+export const setStatusAC = (status: string): SetStatusActionType => ({
   type: SET_STATUS,
   status
 })
 
-export const deletePostAC = (postId) => ({
+type DeletePostActionType = {
+  type: typeof DELETE_POST
+  postId: number
+}
+
+export const deletePostAC = (postId: number):DeletePostActionType => ({
   type: DELETE_POST,
   postId
 })
 
-export const savePhotoSuccessAC = (photos) => ({
+type SavePhotoActionType = {
+  type: typeof SAVE_PHOTO_SUCCESS
+  photos: PhotosType
+}
+
+export const savePhotoSuccessAC = (photos: PhotosType):SavePhotoActionType => ({
   type: SAVE_PHOTO_SUCCESS,
   photos
 })
 
 //получаем профиль юзера и устанавливаем в стейт
-export const getProfileThunkCreator = (userId) => async (dispatch) => {
+export const getProfileThunkCreator = (userId: number) => async (dispatch: any) => {
   const data = await profileAPI.getProfile(userId)
   dispatch(setUserProfileAC(data));   
 }
 
 //получаем статус юзера и устанавливаем в стейт
-export const getStatusThunkCreator = (userId) => async (dispatch) => {
+export const getStatusThunkCreator = (userId: number) => async (dispatch: any) => {
   const data = await profileAPI.getStatus(userId)
   dispatch(setStatusAC(data));
 }
 
 //меняем статус юзера и устанавливаем в стейт
-export const updateStatusThunkCreator = (status) => async (dispatch) => {
+export const updateStatusThunkCreator = (status: string) => async (dispatch: any) => {
   try {
     const data = await profileAPI.updateStatus(status)
     if (data.resultCode === 0) {
@@ -111,7 +170,7 @@ export const updateStatusThunkCreator = (status) => async (dispatch) => {
 }
 
 //загружаем фото профиля
-export const savePhotoThunkCreator = (photo) => async (dispatch) => {
+export const savePhotoThunkCreator = (photo: any) => async (dispatch: any) => {
   const data = await profileAPI.savePhoto(photo)
   if (data.resultCode === 0) {
     dispatch(savePhotoSuccessAC(data.data.photos));
@@ -119,7 +178,7 @@ export const savePhotoThunkCreator = (photo) => async (dispatch) => {
 }
 
 //изменяем данные профиля
-export const saveProfileThunkCreator = (profile) => async (dispatch, getState) => {
+export const saveProfileThunkCreator = (profile: ProfileType) => async (dispatch: any, getState: Function) => {
   const userId = getState().auth.userId
   const data = await profileAPI.saveProfile(profile)
   if (data.resultCode === 0) {
